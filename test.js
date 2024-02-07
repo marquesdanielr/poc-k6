23d64e81269ee74a410f198401f8e7d350e302e1
marquesdanielr <82175768+marquesdanielr@users.noreply.github.com>
2024-02-07 12:41:36 -0300

    teste

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ import { vu } from "./variables.js";
 
 export const options = {
   duration: "1m",
-  vus: ${vu},
+  vus: 50,
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
     http_req_duration: ["p(95)<500"], // 95 percent of response times must be below 500ms
